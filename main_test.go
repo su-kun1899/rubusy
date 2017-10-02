07fcf4d3b687a03a3ab033fc870d3072605568e8
su-kun1899 <higedrum.coz@gmail.com>
2017-10-02 17:40:28 +0900

    change test method name

--- main_test.go
+++ main_test.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func TestSample(t *testing.T) {
+func TestTargetTime_1985_01_07_07_07_00(t *testing.T) {
 	// setup
 	tm := time.Date(1985, 1, 7, 7, 7, 0, 0, time.UTC)
 	expected := "target time: 1985-01-07 07:07:00"
