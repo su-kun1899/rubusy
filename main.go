ba62ab6a15ffa99ae0a2d03e54674c79a8edb09e
su-kun1899 <higedrum.coz@gmail.com>
2017-10-03 17:23:40 +0900

    add string method
    $B=PNO;~J,$+$j$d$9$$$h$&$KJ8;zNs2=%a%=%C%I$rDI2C(B

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 	app.Usage = "tell you which cron will be executed."
 	app.Action = func(c *cli.Context) error {
 		// fmt.Printf("Hello %s\n", c.Args().Get(0))
-		fmt.Println(targetTime(time.Now()))
+		fmt.Println(newTargetTime(time.Now()))
 
 		return nil
 	}
@@ -23,11 +23,22 @@ func main() {
 	app.Run(os.Args)
 }
 
-func targetTime(t time.Time) string {
+type targetTime struct {
+	from time.Time
+	to   time.Time
+}
+
+func (target targetTime) String() string {
+	const format = "2006-01-02 15:04:05"
+	return fmt.Sprintf("from: %s, to: %s", target.from.Format(format), target.to.Format(format))
+}
+
+func newTargetTime(t time.Time) targetTime {
 	const format = "2006-01-02 15:04:05"
 	from := t
 	to := t.Add(time.Duration(1) * time.Hour)
-	return fmt.Sprintf("target time from: %q to: %q", from.Format(format), to.Format(format))
+	// return fmt.Sprintf("target time from: %q to: %q", from.Format(format), to.Format(format))
+	return targetTime{from, to}
 }
 
 func readCrontabFile(fileName string) []string {
--- main_test.go
+++ main_test.go
@@ -7,20 +7,6 @@ import (
 	"time"
 )
 
-func TestTargetTime_1985_01_07_07_07_00(t *testing.T) {
-	// setup
-	tm := time.Date(1985, 1, 7, 7, 7, 0, 0, time.UTC)
-	expected := "target time from: \"1985-01-07 07:07:00\" to: \"1985-01-07 08:07:00\""
-
-	// execute
-	actual := targetTime(tm)
-
-	// assert
-	if actual != expected {
-		t.Fatalf("expected: %q actual: %q\n", expected, actual)
-	}
-}
-
 func TestReadCrontabFile_crontab_example(t *testing.T) {
 	// setup
 	dir, _ := os.Getwd()
@@ -32,6 +18,32 @@ func TestReadCrontabFile_crontab_example(t *testing.T) {
 
 	// assert
 	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("expected: %q but actual: %q\n", expected, actual)
+	}
+}
+
+func TestNewTargetTime_1985_01_07_07_07_00(t *testing.T) {
+	// setup
+	param := time.Date(1985, 1, 7, 7, 7, 0, 0, time.UTC)
+	expectedFrom := param
+	expectedTo := time.Date(1985, 1, 7, 8, 7, 0, 0, time.UTC)
+	expected := targetTime{expectedFrom, expectedTo}
+
+	// execute
+	actual := newTargetTime(param)
+
+	// asert
+	if actual != expected {
+		t.Fatalf("expected: %q but actual: %q\n", expected, actual)
+	}
+}
+
+func TestStringTargetTime(t *testing.T) {
+	target := targetTime{time.Date(1985, 1, 7, 7, 7, 5, 0, time.UTC), time.Date(2017, 10, 3, 17, 18, 9, 0, time.UTC)}
+	expected := "from: 1985-01-07 07:07:05, to: 2017-10-03 17:18:09"
+	actual := target.String()
+
+	if actual != expected {
 		t.Fatalf("expected: %q but actual: %q\n", expected, actual)
 	}
 }
