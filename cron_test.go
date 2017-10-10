dbc528518bcc87d7dd751ca1cd59e6fdd935523a
su-kun1899 <higedrum.coz@gmail.com>
2017-10-10 09:23:40 +0900

    refactoring

--- cron_test.go
+++ cron_test.go
@@ -4,19 +4,15 @@ import "testing"
 import "reflect"
 
 func TestNewCronJob(t *testing.T) {
-	job := "32 17 3 10 Tue /tmp/hoge.sh"
-	expected := CronJob{
-		minute:     []int{32},
-		hour:       []int{17},
-		dayOfMonth: []int{3},
-		month:      []int{10},
-		dayOfWeek:  []int{2},
-		line:       job,
+	jobs := []CronJob{
+		{line: "32 17 3 10 Tue /tmp/hoge.sh", minute: []int{32}, hour: []int{17}, dayOfMonth: []int{3}, month: []int{10}, dayOfWeek: []int{2}},
 	}
 
-	actual := NewCronJob(job)
+	for _, expected := range jobs {
+		actual := NewCronJob(expected.line)
 
-	if !reflect.DeepEqual(actual, expected) {
-		t.Fatalf("expected: %+v but actual: %+v\n", expected, actual)
+		if !reflect.DeepEqual(actual, expected) {
+			t.Fatalf("expected: %+v but actual: %+v\n", expected, actual)
+		}
 	}
 }
