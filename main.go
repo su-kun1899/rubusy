9656bcb0f8f627f19cc2b59ab40c0259d89b8038
su-kun1899 <higedrum.coz@gmail.com>
2017-10-12 18:00:35 +0900

    remove unnecessary methods

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 	"time"
 
@@ -19,17 +18,18 @@ func main() {
 		fileName := c.Args().Get(0)
 		timeCondition := newTargetTime(time.Now())
 		fmt.Println(timeCondition)
-		tasks := searchCronTasks(fileName, &timeCondition)
 
-		if len(tasks) == 0 {
+		jobs := readCrontabFile(fileName)
+		if len(jobs) == 0 {
 			fmt.Println("Probably nothing to do. no cron tasks.")
 			return nil
 		}
-		for _, task := range tasks {
-			// TODO 行番号出して上げてもいいかも
-			fmt.Printf("%s\n", task.line)
+
+		for _, job := range jobs {
+			// TODO 次回実行時間を表示
+			fmt.Printf("%s\n", job.line)
 		}
-		fmt.Printf("Probably %d cron tasks will run.\n", len(tasks))
+		fmt.Printf("Probably %d cron tasks will run.\n", len(jobs))
 
 		return nil
 	}
@@ -75,141 +75,4 @@ func newTargetTime(t time.Time) targetTime {
 	to := t.Add(time.Duration(1) * time.Hour)
 	// return fmt.Sprintf("target time from: %q to: %q", from.Format(format), to.Format(format))
 	return targetTime{from, to}
-}
-
-// crontab一行分の情報を保持する構造体
-type cronTask struct {
-	minute     string
-	hour       string
-	dayOfMonth string
-	month      string
-	dayOfWeek  string
-	line       string
-}
-
-func newCronTask(line string) cronTask {
-	splited := strings.Split(line, " ")
-	return cronTask{
-		minute:     splited[0],
-		hour:       splited[1],
-		dayOfMonth: splited[2],
-		month:      splited[3],
-		dayOfWeek:  splited[4],
-		line:       line,
-	}
-}
-
-func searchCronTasks(fileName string, t *targetTime) []cronTask {
-	fp, err := os.Open(fileName)
-	if err != nil {
-		panic(err)
-	}
-	defer fp.Close()
-
-	tasks := make([]cronTask, 0)
-	scanner := bufio.NewScanner(fp)
-	for scanner.Scan() {
-		line := scanner.Text()
-		if line == "" || strings.HasPrefix(line, "#") {
-			continue
-		}
-		task := newCronTask(line)
-		if ok, _ := filterCronTask(&task, t); ok {
-			tasks = append(tasks, task)
-		}
-	}
-
-	return tasks
-}
-
-func filterCronTask(task *cronTask, target *targetTime) (bool, *cronTask) {
-	// month
-	if !matchMonth(task, target) {
-		return false, nil
-	}
-
-	// day of month
-	if task.dayOfMonth != "*" {
-		intDayOfMonth, _ := strconv.Atoi(task.dayOfMonth)
-		//numFrom, _ := strconv.Atoi(target.from.Format("1"))
-		//numTo, _ := strconv.Atoi(target.to.Format("1"))
-		if !(target.from.Day() <= intDayOfMonth && intDayOfMonth <= target.to.Day()) {
-			return false, nil
-		}
-	}
-
-	return true, task
-}
-
-func matchMonth(task *cronTask, target *targetTime) bool {
-	var taskMonth string
-	var cycle int
-	if strings.Contains(task.month, "/") {
-		monthSplited := strings.Split(task.month, "/")
-		taskMonth = monthSplited[0]
-		cycle, _ = strconv.Atoi(monthSplited[1])
-	} else {
-		taskMonth = task.month
-	}
-
-	numFrom, _ := strconv.Atoi(target.from.Format("1"))
-	numTo, _ := strconv.Atoi(target.to.Format("1"))
-
-	var months []string
-	if taskMonth == "*" {
-		for i := 0; i < 12; i++ {
-			months = append(months, fmt.Sprint(i+1))
-		}
-	} else if strings.Contains(taskMonth, ",") {
-		months = strings.Split(taskMonth, ",")
-	} else if strings.Contains(taskMonth, "-") {
-		months = oldMonthRange(taskMonth)
-	} else {
-		startMonth, _ := strconv.Atoi(taskMonth)
-		endMonth := 12
-		months = make([]string, 0)
-		for i := startMonth; i <= endMonth; i++ {
-			months = append(months, fmt.Sprint(i))
-		}
-	}
-
-	var cycles []int
-	if cycle != 0 {
-		startMonth, _ := strconv.Atoi(months[0])
-		endMonth, _ := strconv.Atoi(months[len(months)-1])
-
-		cycles = make([]int, 0)
-		i := startMonth
-		for i < endMonth {
-			cycles = append(cycles, i)
-			i = i + cycle
-		}
-	} else {
-		cycles = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
-	}
-
-	for _, taskMonth := range months {
-		intMonth, _ := strconv.Atoi(taskMonth)
-		if numFrom <= intMonth && intMonth <= numTo {
-			for _, cycle := range cycles {
-				if intMonth == cycle {
-					return true
-				}
-			}
-		}
-	}
-
-	return false
-}
-
-func oldMonthRange(s string) []string {
-	monthSplited := strings.Split(s, "-")
-	startMonth, _ := strconv.Atoi(monthSplited[0])
-	endMonth, _ := strconv.Atoi(monthSplited[1])
-	months := make([]string, 0)
-	for i := startMonth; i <= endMonth; i++ {
-		months = append(months, fmt.Sprint(i))
-	}
-
-	return months
 }
--- main_test.go
+++ main_test.go
@@ -27,29 +27,6 @@ func TestReadCrontabFile(t *testing.T) {
 	}
 }
 
-func TestReadCrontabFile_crontab_example(t *testing.T) {
-	// setup
-	dir, _ := os.Getwd()
-	filename := dir + "/crontab_example"
-	expected := []cronTask{
-		newCronTask("15 9 * * * /tmp/hoge.sh"),
-		newCronTask("10 10 * * * /tmp/fuga.sh"),
-	}
-
-	targetTime := targetTime{
-		time.Date(2017, 10, 4, 18, 0, 0, 0, time.UTC),
-		time.Date(2017, 10, 4, 19, 0, 0, 0, time.UTC),
-	}
-
-	// execute
-	actual := searchCronTasks(filename, &targetTime)
-
-	// assert
-	if !reflect.DeepEqual(actual, expected) {
-		t.Fatalf("expected: %q but actual: %q\n", expected, actual)
-	}
-}
-
 func TestNewTargetTime_1985_01_07_07_07_00(t *testing.T) {
 	// setup
 	param := time.Date(1985, 1, 7, 7, 7, 0, 0, time.UTC)
@@ -76,98 +53,5 @@ func TestStringTargetTime(t *testing.T) {
 
 	if actual != expected {
 		t.Fatalf("expected: %q but actual: %q\n", expected, actual)
-	}
-}
-func TestNewCrontab(t *testing.T) {
-	crontabLine := "32 17 3 10 Tue /tmp/hoge.sh"
-	expected := cronTask{
-		minute:     "32",
-		hour:       "17",
-		dayOfMonth: "3",
-		month:      "10",
-		dayOfWeek:  "Tue",
-		line:       crontabLine,
-	}
-
-	actual := newCronTask(crontabLine)
-
-	if actual != expected {
-		t.Fatalf("expected: %q but actual: %q\n", expected, actual)
-	}
-}
-
-func TestSearchCronTasks_unmatch(t *testing.T) {
-	// given
-	dir, _ := os.Getwd()
-	testFile := dir + "/crontab_unmatch"
-	targetTime := targetTime{
-		time.Date(2017, 10, 4, 18, 0, 0, 0, time.UTC),
-		time.Date(2017, 10, 4, 19, 0, 0, 0, time.UTC),
-	}
-
-	// when
-	actual := searchCronTasks(testFile, &targetTime)
-
-	if len(actual) != 0 {
-		t.Fatal("actual should return empty slice \n")
-	}
-}
-
-func TestFilterCronTask_match(t *testing.T) {
-	targetTime := targetTime{
-		from: time.Date(2017, 10, 4, 18, 0, 0, 0, time.UTC),
-		to:   time.Date(2017, 10, 4, 19, 0, 0, 0, time.UTC),
-	}
-	cronTasks := []cronTask{
-		newCronTask("30 * * * * /tmp/hoge.sh"),
-		newCronTask("30 * * 10 * /tmp/hoge.sh"),
-		newCronTask("30 * * 8,9,10 * /tmp/hoge.sh"),
-		newCronTask("30 * * 9,10,11 * /tmp/hoge.sh"),
-		newCronTask("30 * * 10,11,12 * /tmp/hoge.sh"),
-		newCronTask("30 * * 8-10 * /tmp/hoge.sh"),
-		newCronTask("30 * * 9-11 * /tmp/hoge.sh"),
-		newCronTask("30 * * 10-12 * /tmp/hoge.sh"),
-		newCronTask("30 * * 10/2 * /tmp/hoge.sh"),
-		newCronTask("30 * * 4/2 * /tmp/hoge.sh"),
-		newCronTask("30 * * 6/2 * /tmp/hoge.sh"),
-		newCronTask("30 * * 1/3 * /tmp/hoge.sh"),
-		newCronTask("30 * * */3 * /tmp/hoge.sh"),
-	}
-
-	for _, task := range cronTasks {
-		ok, actual := filterCronTask(&task, &targetTime)
-		if !ok {
-			t.Fatalf("crontTask should match filter condition: %q \n", task)
-		}
-		if actual != &task {
-			t.Fatalf("actual should match cronTask: %q \n", task)
-		}
-	}
-}
-
-func TestFilterCronTask_unmatch(t *testing.T) {
-	targetTime := targetTime{
-		from: time.Date(2017, 10, 4, 18, 0, 0, 0, time.UTC),
-		to:   time.Date(2017, 10, 4, 19, 0, 0, 0, time.UTC),
-	}
-	cronTasks := []cronTask{
-		newCronTask("30 * * 11 * /tmp/hoge.sh"),
-		newCronTask("30 * * 8,9,12 * /tmp/hoge.sh"),
-		newCronTask("30 * * 1-9 * /tmp/hoge.sh"),
-		newCronTask("30 * * 9/2 * /tmp/hoge.sh"),
-		newCronTask("30 * * 3/2 * /tmp/hoge.sh"),
-		newCronTask("30 * * 2/3 * /tmp/hoge.sh"),
-		newCronTask("30 * * */2 * /tmp/hoge.sh"),
-		newCronTask("30 * * 1-9/3 * /tmp/hoge.sh"),
-	}
-
-	for _, task := range cronTasks {
-		ok, actual := filterCronTask(&task, &targetTime)
-		if ok {
-			t.Fatalf("crontTask should not match filter condition: %q \n", task)
-		}
-		if actual != nil {
-			t.Fatal("actual should return nil \n")
-		}
 	}
 }
